230e4c410f1aa28847f989a138ac784753610c4e
François Bélanger <f.belanger@samsung.com>
2021-08-27 00:12:43 -0400

    add Dockerfile & docker-compose.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "application#status"
+  get "/status", to: "application#status"
   post "/graphql/user_service", to: "graphql#execute_user"
   post "/graphql/item_service", to: "graphql#execute_item"
   post "/graphql/role_service", to: "graphql#execute_role"
